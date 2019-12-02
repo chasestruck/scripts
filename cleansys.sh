51303fd743bc751d48e313e44bf1f5d12ced7f77
Chase <58320619+chasestruck@users.noreply.github.com>
2019-12-02 11:23:43 -0600

    update cleansys setup description

--- cleansys.sh
+++ cleansys.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
+##################################################################################
+# CLEANSYS - THE STUPID SYSTEM MAINTENANCE SCRIPT                                #
+#                                                                                #
+# Copyright (C) 2019 Gizmoz                                                      #
+#                               ]                                                #
+# cleansys: Runs helpful system maintenance commands all in one script!          #
+# (for DEBIAN-BASED (apt) systems ONLY)                                          #
+#                                                                                #
+# This program is free software: you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by           #
+# the Free Software Foundation, either version 3 of the License, or              #
+# (at your option) any later version.                                            #
+#	                                                                         #
+# This program is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+# GNU General Public License for more details.                                   #
+#	                                                                         #
+# You should have received a copy of the GNU General Public License              #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.           #
+##################################################################################
 
-##############################################################################
-# CLEANSYS - THE STUPID SYSTEM MAINTENANCE SCRIPT                            #
-#                                                                            #
-# Copyright (C) 2019 Gizmoz                                                  #
-#                                                                            #
-# cleansys: Runs helpful system maintenance commands all in one script!      #
-# (for DEBIAN-BASED (apt) systems ONLY)                                      #
-#                                                                            #
-# This program is free software: you can redistribute it and/or modify       #
-# it under the terms of the GNU General Public License as published by       #
-# the Free Software Foundation, either version 3 of the License, or          #
-# (at your option) any later version.                                        #
-#	                                                                     #
-# This program is distributed in the hope that it will be useful,            #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-# GNU General Public License for more details.                               #
-#	                                                                     #
-# You should have received a copy of the GNU General Public License          #
-# along with this program. If not, see <http://www.gnu.org/licenses/>.       #
-##############################################################################
-
-##############################################################################
-# SETUP - to use this program, put it in a directory (via git clone or just  #
-# copy paste it as a file and chmod it as an exe). Then you can cd to that   #
-# directory and do sudo ./cleansys (or whatever you named it). To use it as  #
-# a bash command, go to the directory you have it in and do                   #
-# sudo cp cleansys /usr/bin/. If it does not work, google it, then try       #
-# turning it off and back on.                                                #
-##############################################################################
+##################################################################################
+# SETUP: {if you have done this stuff before, skip this becuase its really easy} #
+# 1. Put the repo into a local directory (via git clone or pasting in the file)  #
+# 2. cd to the new directory [cd ~/my_cleansys_dir]                              #
+# 3. use chmod to make it an execuable [sudo chmod +x cleansys.sh]               #
+# 	a. you can use the mv command here to remove the .sh if you wish         #
+# 4. run it as root [sudo ./cleansys.sh]                                         #
+# HOW TO MAKE IT A BASH COMMAND                                                 #
+# 1. run steps above                                                             #
+# 2. run [sudo cp cleansys /usr/bin/                                             #
+# 3. cd to home [cd]                                                             #
+# 4. run [sudo cleansys]                                                         #
+##################################################################################
 
 if [ "$EUID" -ne 0 ] # checks for root, exits if not root.
   then echo -e "\e[1m\e[31mRun this as root (sudo)"
