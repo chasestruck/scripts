c9226e1eab0f621dd03862ac75485a65ec3ae421
Chase Struck <chasestruck@protonmail.com>
2020-03-23 00:39:36 -0500

    Make usage message more clear

--- get-weather.sh
+++ get-weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Gets the weather via curling wttr.in
-# Usage: get-weather [CITYNAME] (providing no args gets local forcast via IP)
+# Usage: get-weather [CITYNAME] (if no city is provided, it will use your IP as the location)
 
 # Any Copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/ 
